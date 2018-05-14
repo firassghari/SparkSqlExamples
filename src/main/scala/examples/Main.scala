86422f6ad3f78c5208ba376f60467fc1cc95178a
firas <firas.sghari@yahoo.fr>
2018-05-14 15:00:05 +0200

    join 2 dataframes

--- src/main/scala/examples/Main.scala
+++ src/main/scala/examples/Main.scala
@@ -4,6 +4,7 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object Main {
   def main(args: Array[String]): Unit = {
+
     val spark = org.apache.spark.sql.SparkSession.builder
       .master("local")
       .appName("Spark SQL examples")
@@ -12,14 +13,20 @@ object Main {
     val schools = readCV("src/main/resources/Schools.csv",spark)
     val players = readCV("src/main/resources/SchoolsPlayers.csv",spark)
     val AvgYearMinPerSchoolId = getAvgYearMinPerSchoolId(players, spark)
-    System.out.println(AvgYearMinPerSchoolId.show())
+  //  System.out.println(AvgYearMinPerSchoolId.show())joinSchoolAndSchoolPlayers(schools,players,spark).show()
 
-
+  }
+  def joinSchoolAndSchoolPlayers(schools:DataFrame,schoolPlayers:DataFrame , session: SparkSession):DataFrame =
+  {
+    import org.apache.spark.sql.functions._
+    schoolPlayers.join(schools , schoolPlayers.col("schoolID")===schools.col("schoolID"))
   }
   def getAvgYearMinPerSchoolId(schools:DataFrame , session:SparkSession): DataFrame =
   {
-    schools.createOrReplaceTempView("schoolplayers")
-    return session.sql("select schoolID , avg(yearMin) from schoolplayers group by schoolID ")
+    import org.apache.spark.sql.functions._
+
+    schools.groupBy("schoolID" ).agg(avg("yearMin").as("hello"))
+
   }
   def readCV (path:String , session:SparkSession ):DataFrame  =
   {
